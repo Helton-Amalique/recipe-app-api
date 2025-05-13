2c49ba16d67da13fae3fffc1980a240f6b48f179
Xipoko <xipoko@kali.xipoko>
2025-05-13 18:49:40 +0200

    add models migrations e mais

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,13 +1,13 @@
-"""
-    #"""'Test for models'"""
- from django.test import TestCase
- from django.contrib.auth import get_user_model
+"""Test for models """
 
- class ModelsTests(TestCase):
-    # Test models"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-"""    def test_create_user_with_email_sucessful(self):
-        #"""'Este teste cria user com email com sucesso'"""
+class ModelsTests(TestCase):
+    """ Test models"""
+
+    def test_create_user_with_email_sucessful(self):
+        """Este teste cria user com email com sucesso"""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
@@ -17,4 +17,3 @@
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-"""
