3dd87d37d3b8dfc25e28ddd634c52a38aa31264d
Xipoko <xipoko@kali.xipoko>
2025-05-14 18:33:32 +0200

    confg superuser django

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,47 @@
+"""
+Teste para as modificacoes de admin
+"""
+
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class AdminSiteTests(TestCase):
+    """Testes para Django o admin"""
+
+    def setUp(self):
+        """Configura o usuario e o cliente"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User',
+        )
+
+    def test_users_list(self):
+        """Testa a lista de usuarios"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """"Testa a pagina de edicao de usuario"""
+        url =  reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Testa a pagina de criacao de usuario"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
