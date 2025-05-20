e9fb7e5900d814c0c92b18a1179e9d488660171c
Helton Amalique <helton_88@hotmail.com>
2025-05-20 06:42:13 +0200

    config user Api acertand lint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -32,7 +32,7 @@ class AuthTokenSerializer(serializers.Serializer):
     """"""
     email = serializers.EmailField()
     password = serializers.CharField(
-        style = {'input_type': 'password'},
+        style= {'input_type':'password'},
         trim_whitespace = False,
     )
 
